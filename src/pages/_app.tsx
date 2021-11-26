fcc087387e820cfa7b247f1eebf8d151ebc965d6
Augusto Lima <augustolimads@gmail.com>
2021-11-26 15:51:16 -0300

    add provider and cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,29 @@
 import { ThemeProvider } from "@material-ui/styles";
 import type { AppProps } from "next/app";
+import { CookiesProvider } from "react-cookie";
+import { Provider } from "react-redux";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Head from "next/head";
+import store from "src/store";
 import theme from "src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Head>
-        <title>Boilerplate next material</title>
-        <meta
-          name="description"
-          content="A simple project starter to work with typescript, React, NextJs, Redux Toolkit and Material UI"
-        />
-      </Head>
-      <Component {...pageProps} />;
-    </ThemeProvider>
+    <Provider store={store}>
+      <CookiesProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Head>
+            <title>Boilerplate next material</title>
+            <meta
+              name="description"
+              content="A simple project starter to work with typescript, React, NextJs, Redux Toolkit and Material UI"
+            />
+          </Head>
+          <Component {...pageProps} />;
+        </ThemeProvider>
+      </CookiesProvider>
+    </Provider>
   );
 }
 
